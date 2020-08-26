6b54a67d957039a07fb4f02b3600d45a6c81d3e2
manuel <manuel@sahores.com>
2020-08-26 19:28:04 -0300

    Use Context + local storage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Axios from 'axios';
 import Home from './components/pages/Home';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Header from './components/layout/Header';
+import UserContext from './context/UserContext';
 import "./style.css";
 
+const baseUrl = "http://localhost:5000";
 //rafce
 const App = () => {
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined
+  });
+  useEffect(()=>{
+    const checkLoggedIn = async()=> {
+      let token = localStorage.getItem("auth-token");
+      if(token === null) {
+        localStorage.setItem("auth-token", "");
+        token = "";
+      }
+      const tokenRes = await Axios.post(baseUrl + '/users/tokenIsValid', null, {
+        headers: {"x-auth-token": token}
+      })
+      if(tokenRes.data){
+        const userRes = await Axios.get(baseUrl + "/users", {headers: {"x-auth-token": token}});
+        console.log("User Res:", userRes);
+        setUserData({
+          token, 
+          user: userRes.data
+        })
+      } else {
+        console.log("Not logged in!");
+      }
+    }
+    checkLoggedIn();
+  }, []);
   return <>
     <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route path="/" component={Home} exact/>
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-      </Switch>
+      <UserContext.Provider value={{userData, setUserData}}>
+        <Header />
+        <Switch>
+          <Route path="/" component={Home} exact/>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+        </UserContext.Provider>
     </BrowserRouter>
   </>
 }
